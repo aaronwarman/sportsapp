350311cd6d6d980f27328e65a3aa5140881de634
Aaron Warman <aaronwarman@gmail.com>
2016-07-08 17:00:47 -0400

    cloned API, created new functions

--- code.js
+++ code.js
@@ -1,23 +1,42 @@
-var StattleshipAPI = require('node-stattleship');
+var StattleshipAPI = require('./node-stattleship');
+var flatMap = require('./flatmap');
+var Q = require('Q');
 
 var stattleship = new StattleshipAPI('940b6b0cd984eaf7e4ee15a509522f1b');
 
-var season = {
- season_id: 'mlb-2016'
+function getOptions (league, season, team, date) {
+  return {
+    season_id: league + "-" + season,
+    team_id: league + "-" + team,
+    on: date
+  };
 };
 
-var gameToday = {
-  on: 'today'
+function getGames (stattleship, sport, league, season, team, date) {
+  if (team.constructor  === Array) {
+    return getMultiple (stattleship, sport, league, season, team, date);
+  }
+  return getSingle (stattleship, sport, league, season, team, date);
 };
 
-stattleship.teams('baseball', 'mlb', season).then(function(teams) {
-  teams.forEach(function(element, index, array) {
-    console.log(element.name);
+function getMultiple (stattleship, sport, league, season, teams, date) {
+  return Q.all(teams.flatMap(function(team) {
+    return stattleship.games(sport, league, getOptions(league, season, team, date));
+  }));
+};
+
+function getSingle (stattleship, sport, league, season, team, date) {
+  return stattleship.games(sport, league, getOptions(league, season, team, date));
+};
+
+getGames(stattleship, 'baseball', 'mlb', '2016', ['was','min'], 'today').then(function(games) {
+  games.forEach(function(res) {
+    res.map(function(v) { console.log(v.name) });
   });
 });
 
-stattleship.games('baseball', 'mlb', gameToday).then(function(games) {
-  games.forEach(function(element, index, array) {
-    console.log(element.name);
+getGames(stattleship, 'baseball', 'mlb', '2016', 'nyy', 'today').then(function(games) {
+  games.forEach(function(res) {
+    console.log(res.name);
   });
 });
